5ba32c9acab815cdb2e797e90dafe4eb26da6ae5
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-03-02 10:30:00 -0500

    feat: refactoring

---
+++ src/constants/homeOptions.ts
@@ -0,0 +1,22 @@
+const homeOptions = [
+  {
+    name: 'Cats',
+    title: 'Cats',
+    image:
+      'https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
+  },
+  {
+    name: 'Dogs',
+    title: 'Dogs',
+    image:
+      'https://images.unsplash.com/photo-1568393691622-c7ba131d63b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
+  },
+  {
+    name: 'Country',
+    title: 'Country',
+    image:
+      'https://images.unsplash.com/photo-1446482972539-0ed52b3e9520?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+];
+
+export default homeOptions;
--- src/views/home/indext.tsx
+++ src/views/home/indext.tsx
@@ -1,37 +1,26 @@
 import { View, StyleSheet, Image, TouchableHighlight } from 'react-native';
 import Separator from '../../common/separator';
+import homeOptions from '../../constants/homeOptions';
 
 const Home = ({ navigation }) => {
-  const moving = () => {};
   return (
     <View style={styles.container}>
-      <TouchableHighlight
-        style={styles.image}
-        onPress={() => navigation.navigate('Dogs')}
-      >
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://images.unsplash.com/photo-1503844281047-cf42eade5ca5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80',
-          }}
-        />
-      </TouchableHighlight>
-      <Separator />
-      <TouchableHighlight>
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://images.unsplash.com/photo-1568393691622-c7ba131d63b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80',
-          }}
-        />
-      </TouchableHighlight>
-      <Separator />
-      <Image
-        style={styles.image}
-        source={{
-          uri: 'https://images.unsplash.com/photo-1446482972539-0ed52b3e9520?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-        }}
-      />
+      {homeOptions.map(({ name, image }, index) => (
+        <>
+          <TouchableHighlight
+            style={styles.image}
+            onPress={() => navigation.navigate(name)}
+          >
+            <Image
+              style={styles.image}
+              source={{
+                uri: image,
+              }}
+            />
+          </TouchableHighlight>
+          {index !== homeOptions.length - 1 && <Separator />}
+        </>
+      ))}
     </View>
   );
 };
